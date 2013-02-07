6f267387d14d1257b83ba307c5e1ee16d038719f
Rikus Goodell <rikus@rikus.net>
2013-02-07 04:12:12 +0000

    Version 0.2.2

--- make_tarball.sh
+++ make_tarball.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-version=`perl -MBc125At::Version -e 'print "$Bc125At::Version::version\n"'`
+version=`perl -Ilib -MBc125At::Version -e 'print "$Bc125At::Version::version\n"'`
 
 mkdir ../tarballs 2>/dev/null
 
 if [ -e ../tarballs/bc125at-perl-$version -o -e ../tarballs/bc125at-perl-$version.tar.gz ]; then
-    echo already exists
+    echo $version tarball already exists
     exit 1
 fi
 mkdir -p ../tarballs/bc125at-perl-$version/{lib,t}
 mkdir ../tarballs/bc125at-perl-$version/lib/Bc125At
 cp -pf README ChangeLog MANIFEST Makefile.PL ../tarballs/bc125at-perl-$version/
 cp -pf bc125at-perl ../tarballs/bc125at-perl-$version/
+cp -pf lib/*.pm ../tarballs/bc125at-perl-$version/lib/
 cp -pf lib/Bc125At/*.pm ../tarballs/bc125at-perl-$version/lib/Bc125At/
 cp -pf t/*.t ../tarballs/bc125at-perl-$version/t/
 
